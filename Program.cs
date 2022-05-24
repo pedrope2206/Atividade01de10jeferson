0d0562388178ccf1462c1365bc94db30faf13a32
pedrope2206 <pivettapedro93@gmail.com>
2022-05-24 19:27:28 -0300

    Atividade01de10jeferson

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//Atividade fieta em sala de aula
+int idadeMin =20;
+int idadeMAX =50;
+int mediaAluno =0;
+
+mediaAluno =  (idadeMAX + idadeMin) /2 ;
+
+Console.WriteLine ($"A média de idade é: {mediaAluno}");
+/*
+//Atividade dinâmica passada pelo professor
+int idadeMin =0;
+int idadeMAX =0;
+int mediaAluno =0;
+
+mediaAluno =  (idadeMAX + idadeMin) /2 ;
+
+Console.WriteLine("Qual é a idade do aulo mais novo da faculdade?");
+idadeMin = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Qual é a idade do aulo mais velho da faculdade?");
+idadeMAX = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine ($"A média de idade é: {mediaAluno}");*/
+
